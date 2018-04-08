79a36ee44191b00900b8e6b43f04adee1adcc9d9
Omar <x64x64x64@gmail.com>
2018-04-08 18:52:03 -0300

    IMPLEMENTACION, CORRECCION Y TESTEO DE DETALLE ACTIVITY Y IMAGE FRAGMENT. CAMBIO DE IMAGENES DE RES POR MDPI

--- app/src/main/java/com/ladoe/rocker/DetalleActivity.java
+++ app/src/main/java/com/ladoe/rocker/DetalleActivity.java
@@ -1,13 +1,75 @@
 package com.ladoe.rocker;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.ladoe.rocker.Constantes.CLAVES;
+import com.ladoe.rocker.Entidades.Publicacion;
+import com.ladoe.rocker.Entidades.SubTipos.Imagen;
+import com.ladoe.rocker.Fragments.ImagenFragment;
+import com.ladoe.rocker.Patrones.PubFactory;
+import com.ladoe.rocker.Patrones.SectionsPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetalleActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private Publicacion publicacion;
+    private SectionsPagerAdapter pagerAdapter;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
+
+        obtenerPublicacion();
+
+        mViewPager=findViewById(R.id.myViewPager);
+
+        pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        generarFragmentImagenes();
+        //generarFragmentVideo();
+
+        mViewPager.setAdapter(pagerAdapter);
     }
+
+    private void obtenerPublicacion(){
+        Bundle extras = getIntent().getExtras();
+        int id = extras.getInt(CLAVES.ID);
+
+        List<Publicacion> listadoCompleto=new ArrayList<>();
+        listadoCompleto.addAll(PubFactory.getEstiloDeVidaList());
+        listadoCompleto.addAll(PubFactory.getShowsYEventosList());
+        listadoCompleto.addAll(PubFactory.getServiciosProfesionalesList());
+        listadoCompleto.addAll(PubFactory.getVentaDeInstrumentosList());
+        listadoCompleto.addAll(PubFactory.getSalasYEstudiosList());
+
+        for (Publicacion pub: listadoCompleto) {
+            if(pub.getDatosBasicos().getId()==id){
+                publicacion=pub;
+            }
+        }
+
+    }
+
+    private void generarFragmentImagenes(){
+        if(publicacion!=null){
+            if(publicacion.getImagenList().size()==0){
+                mViewPager.setVisibility(View.GONE);
+            }else {
+                for (Imagen imagen : publicacion.getImagenList()) {
+                    pagerAdapter.addFragment(ImagenFragment.newInstance(imagen.getUri(), null));
+
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/ladoe/rocker/Patrones/SectionsPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.ladoe.rocker.Patrones;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragments = new ArrayList<>();
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    public void addFragment(android.support.v4.app.Fragment fragment) {
+        mFragments.add(fragment);
+
+    }
+
+
+}
